9ae999fca84ba58be25df0bb12b3f42b88d29973
= <kangju1996@naver.com>
2021-07-08 09:48:15 +0900

    유빈아 로그인화면 기능넣어줘

--- app/src/main/java/com/smhrd/js/Login.java
+++ app/src/main/java/com/smhrd/js/Login.java
@@ -1,15 +1,51 @@
 package com.smhrd.js;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
+    private EditText edt_login_id, edt_login_pw;
+    private Button btn_login, btn_join_go, btn_id_pw_find;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edt_login_id = findViewById(R.id.edt_login_id);
+        edt_login_pw = findViewById(R.id.edt_login_pw);
+
+        btn_login = findViewById(R.id.btn_login);
+        btn_join_go = findViewById(R.id.btn_join_go);
+        btn_id_pw_find = findViewById(R.id.btn_id_pw_find);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //null
+
+
+            }
+        });
+
+        btn_join_go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn_id_pw_find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
--- app/src/main/java/com/smhrd/js/join.java
+++ app/src/main/java/com/smhrd/js/join.java
@@ -9,13 +9,14 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class join extends AppCompatActivity {
+    EditText edt_join_id, edt_join_pw, edt_join_name, edt_join_phone, edt_join_email, edt_join_nickname;
+    Button btn_join_create;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
-
 
     }
 }
